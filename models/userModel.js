56570ec07ede99bae76179ccf99e5963d65f3a95
SaschaGoebbels <109481671+SaschaGo2003@users.noreply.github.com>
2023-02-26 09:28:17 +0100

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: { required: [true, 'Bitte Name eingeben !'], type: String, trim: true },
@@ -6,19 +7,26 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Bitte Email eingeben !'],
     type: String,
     trim: true,
-    unique: [true, 'Die Emailadresse ist schon vorhanden !']
-  }
-  // password: {
-  //   required: [true, 'Bitte Passwort eingeben!'],
-  //   type: String,
-  //   trim: true
-  // },
-  // passwordConfirm: { required: true, type: String, trim: true },
-  // settings: { type: String, ref: 'Settings' },
-  // recipeList: [{ type: Array, ref: 'Recipe' }],
-  // shoppingList: [{ type: Array, ref: 'ShoppingList' }],
-  // weeklyPlan: [{ type: Array, ref: 'WeeklyPlan' }],
-  // superUser: { type: Boolean, default: false }
+    unique: [true, 'Die Emailadresse ist schon vorhanden !'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Bitte eine gültige Emailadresse eingeben!']
+  },
+  photo: String,
+  password: {
+    required: [true, 'Bitte Passwort eingeben!'],
+    type: String,
+    trim: true,
+    minlength: [4, 'Passwort muss mindestens 4 Zeichen lang sein!']
+  },
+  passwordConfirm: {
+    required: [true, 'Bitte Passwort bestätigen!'],
+    type: String
+  },
+  settings: { type: String, ref: 'Settings' },
+  recipeList: [{ type: Array, ref: 'Recipe' }],
+  shoppingList: [{ type: Array, ref: 'ShoppingList' }],
+  weeklyPlan: [{ type: Array, ref: 'WeeklyPlan' }],
+  superUser: { type: Boolean, default: false }
 });
 
-exports.Users = mongoose.model('User', userSchema);
+exports.User = mongoose.model('User', userSchema);
