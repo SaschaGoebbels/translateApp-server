868dbff8c0f598e0bb26322f8cb7055de1c3cd0d
SaschaGoebbels <109481671+SaschaGo2003@users.noreply.github.com>
2023-02-26 11:17:50 +0100

    error handling

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  // console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,5 +22,19 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/recipe', recipeRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  const err = new Error(`Url: ${req.originalUrl} nicht gefunden!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+  // res.status(404).json({ message: `Url: ${req.originalUrl} nicht gefunden!` });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+});
 
 module.exports = app;
---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+// const User = require('../models/userModel');
+
+// export.signup = async (req, res, next) => {
+//     try {
+//         const newUser = await User.create(req.body);
+//         res.status(201).json({status: success, user: newUser});
+
+//     } catch (error) {
+//     res.status(500).json({err: error,message:error.message});
+//     }
+// }
